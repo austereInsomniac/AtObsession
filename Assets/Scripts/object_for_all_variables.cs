4116ad6209bc0c445bbcd0cba9984f24223a2176
ConnorMckillip22 <mckillipc8191@my.uwstout.edu>
2023-10-06 11:40:34 -0500

    object script

---
+++ Assets/Scripts/object_for_all_variables.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_for_all_variables : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
