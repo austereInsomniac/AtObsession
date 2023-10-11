c7f41c52eeb3c7b05953ff65e0c817398390d9ec
ConnorMckillip22 <mckillipc8191@my.uwstout.edu>
2023-10-11 12:13:51 -0500

    Update object_for_all_variables.cs

--- Assets/Scripts/object_for_all_variables.cs
+++ Assets/Scripts/object_for_all_variables.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+class ActivityVariables
+{
+    int wellness;
+    int time;
+    int reputation;
+    int subscribers;
+    int end;
+    double money;
+    public ActivityVariables(int changeWellness, int changeTime, int changeRep, int changeSubs, int changeEnd, double changeMoney)
+    {
+        wellness = changeWellness;
+        time = changeTime;
+        reputation = changeRep;
+        subscribers = changeSubs;
+        end = changeEnd;
+        money = changeMoney;
+    }
+}
 public class object_for_all_variables : MonoBehaviour
 {
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        
     }
 
     // Update is called once per frame
