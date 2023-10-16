fecfc050f43814af44b3d16a761402644d4cfe15
Mackenzie Kuska <silverwood135831@gmail.com>
2023-10-16 12:08:53 -0500

    moveThroughDoor

--- Assets/Scripts/move_through_door.cs
+++ Assets/Scripts/move_through_door.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
+=======
+using System.Runtime.CompilerServices;
+using UnityEditor.SceneTemplate;
+>>>>>>> Stashed changes
 using UnityEngine;
 
 public class move_through_door : MonoBehaviour
 {
+    private Vector3 storedLocation = new Vector3(2000, 0, 0);
+    private Vector3 cameraLocation = new Vector3(0, 0, 0);
 
+<<<<<<< Updated upstream
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,22 @@ public class move_through_door : MonoBehaviour
 
     // Update is called once per frame
     public void OnMouseDown()
+=======
+    [SerializeField]
+    private GameObject other; 
+
+    private void moveLocation(int moveFrom, int moveTo)
+    {
+       Vector3 locationFrom = new Vector3(moveFrom, 0, 0);
+       Vector3 locationTo = new Vector3(moveTo, 0, 0);
+        // move this away
+        this.transform.position = storedLocation;
+        // move away olf from camera
+        other.transform.position = cameraLocation;
+    }
+
+   /* public void OnMouseDown()
+>>>>>>> Stashed changes
     {
         Vector3 pos = new Vector3(0, 0, 5);
         Debug.Log("Click");
@@ -29,6 +53,8 @@ public class move_through_door : MonoBehaviour
                 Debug.Log("Something was clicked");
                 Camera.main.transform.position = pos;
             }
+
+
         } 
-    }
+    }*/
 }
