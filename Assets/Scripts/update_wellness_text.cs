16a46aad4b22809c4e08d4f3a68c06d63d41b668
Mackenzie Kuska <silverwood135831@gmail.com>
2023-10-18 07:41:27 -0500

    added day and time hud

--- Assets/Scripts/update_wellness_text.cs
+++ Assets/Scripts/update_wellness_text.cs
@@ -5,23 +5,40 @@ using TMPro;
 
 public class update_text : MonoBehaviour
 {
-    // get text object
-    [SerializeField]
-    private TMP_Text text;
-
-    // get player
+    // get text boxes
     GameObject player;
+    TMP_Text dayText;
+    TMP_Text timeText;
+    TMP_Text wellnessText;
 
     void Awake() {
-        // Player in "" is the name of the Game Object with the game_state script
-        player = GameObject.Find("Player");
+        dayText = GameObject.Find("Day Text").GetComponent<TMP_Text>();
+        timeText = GameObject.Find("Time Text").GetComponent<TMP_Text>();
+        wellnessText = GameObject.Find("Wellness Text").GetComponent<TMP_Text>();
+
+        // add delegates
+        GetComponent<game_state>().addOnWellnessChange(updateWellnessText);
+        GetComponent<game_state>().addOnTimeChange(updateTimeText);
     }
 
-    void Update()
+    void updateWellnessText(int oldW, int newW)
     {
         // update the text
-        string updateText = "Wellness: " + player.GetComponent<game_state>().getWellness();
-        text.SetText(updateText);
+        string updateText = "Wellness: " + GetComponent<game_state>().getWellness();
+        wellnessText.SetText(updateText);
+    }
+
+    void updateTimeText(int oldTime, int newTime)
+    {
+        // update the time text
+        string updateText = "Time: " + (GetComponent<game_state>().getTime() / 60) + ":" 
+                            + (GetComponent<game_state>().getTime() % 60);
+        timeText.SetText(updateText);
+        // issue: minute ending in 0 will only display one digit
+
+        // update the day text
+        updateText = "Day: " + GetComponent<game_state>().getDay();
+        dayText.SetText(updateText);
     }
 
 }
