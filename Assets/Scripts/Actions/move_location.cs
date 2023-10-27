23b652ef00b60d67711b219cd262e7a94664b3b7
Mackenzie Kuska <silverwood135831@gmail.com>
2023-10-27 11:57:37 -0500

    THE GAME IS FIXED

--- Assets/Scripts/Actions/move_location.cs
+++ Assets/Scripts/Actions/move_location.cs
@@ -20,15 +20,14 @@ public class move_location : MonoBehaviour
     private GameObject player;
     private GameObject bedroom;
     private GameObject bedroomCanvas;
-    //private GameObject blocker;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
         bedroom = GameObject.Find("Bedroom");
         bedroomCanvas = GameObject.Find("Bedroom Canvas");
-        //blocker = GameObject.Find("Raycast Blocker");
     }
+
     public void moveLocation(GameObject other_, GameObject otherCanvas_, GameObject this_, GameObject thisCanvas_)
     {
         // move this room away
