00de3e68f161e6b04129a07a0a33f2f708097cf9
Riley DuLak <dulakr8679@my.uwstout.edu>
2023-11-01 10:34:17 -0500

    test

---
+++ Assets/Scripts/force_sleep.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force_sleep : MonoBehaviour
+
+{
+    [SerializeField]
+    private int time;
+    [SerializeField]
+    private int wellness;
+    // Start is called before the first frame update
+    public void forceSleep()
+    {
+        if(time == 4)
+        {
+            time = 8;
+            wellness -= 5;
+            //Move to the bedroom
+
+        }
+
+    }
+
+    //for testing only
+    public void increaseTime_TESTING()
+    {
+        if(time == 23)
+        {
+            time = 1;
+        }
+        else 
+        { 
+            time++; 
+        }
+       
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
