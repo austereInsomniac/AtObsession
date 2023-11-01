c47d13de3ed6fd844e2428125db32811a097ab57
DrakeMitchell <mitchelld7373@my.uwstout.edu>
2023-11-01 11:45:15 -0500

    Hover effect implemented

--- Assets/Scripts/Self Running Script/stalker_prototype_script.cs
+++ Assets/Scripts/Self Running Script/stalker_prototype_script.cs
@@ -61,7 +61,6 @@ public class stalker_prototype_script : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Debug.Log(eventNum);
         int wellness = player.GetComponent<game_state>().getWellness();
         int day = player.GetComponent<game_state>().getDay();
         // Check if it's time to end the event.
