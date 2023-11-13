743310ca3f036d5c8fb66b6d886f7be11fbc378a
Mackenzie Kuska <silverwood135831@gmail.com>
2023-11-13 13:07:47 -0600

    asset toggling + multiple highlight handling

--- Assets/Scripts/swap_assets.cs
+++ Assets/Scripts/swap_assets.cs
@@ -8,11 +8,14 @@ public class swap_assets : MonoBehaviour
 {
     SpriteRenderer other;
     SpriteRenderer thisObject;
+    asset_highlighter highlightManager;
 
     public void Start()
     {
-        other = GetComponentsInChildren<SpriteRenderer>()[0];
+        // the alternate object must be the second child behind the highlight
+        other = GetComponentsInChildren<SpriteRenderer>()[2];
         thisObject = GetComponent<SpriteRenderer>();
+        highlightManager = GetComponent<asset_highlighter>();
     }
 
     // swaps the visibility of this objectc and the cooresponding object
@@ -20,6 +23,22 @@ public class swap_assets : MonoBehaviour
     public void swapObjects()
     {
         other.enabled = !other.enabled;
-        this.enabled = !thisObject.enabled;   
+        thisObject.enabled = !thisObject.enabled;   
+    }
+
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider == this.GetComponent<BoxCollider2D>())
+            {
+                swapObjects();
+                highlightManager.swapHighlight();
+            }
+        }
     }
 }
