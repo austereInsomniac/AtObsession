62dfec682b397a578f6d416e3f6df398e2981c06
DrakeMitchell <mitchelld7373@my.uwstout.edu>
2023-11-15 11:14:12 -0600

    still testing :<{

--- Assets/Tests/VideoMakingTests.cs
+++ Assets/Tests/VideoMakingTests.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
-public class NewTestScript
+public class VideoMakingTests
 {
 
     public GameObject player;
@@ -23,55 +23,56 @@ public class NewTestScript
     [UnityTest]
     public IEnumerator VideoMakingTestReputation()
     {
-        
+
         player = GameObject.FindGameObjectWithTag("MainCamera");
         computerCanvas = GameObject.Find("Computer Canvas");
         videoMaking = player.GetComponent<video_making>();
+        game_state gameState = player.GetComponent<game_state>();
 
         Assert.IsNotNull(player);
         Assert.IsNotNull(computerCanvas);
         Assert.IsNotNull(videoMaking);
 
 
-        Assert.That(player.GetComponent<game_state>().getReputation() == 20);
+        Assert.That(gameState.getReputation() == 20);
         for (int i = 1; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
+            gameState.updateWellness(100);
             videoMaking.makeVideo(1);
-            Assert.That(player.GetComponent<game_state>().getReputation(), Is.EqualTo(20 + (i * 3)));
+            Assert.That(gameState.getReputation(), Is.EqualTo(20 + (i * 3)));
         }
 
-        player.GetComponent<game_state>().updateReputation(-27);
+        gameState.updateReputation(-27);
 
         for (int i = 1; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
+            gameState.updateWellness(100);
             videoMaking.makeVideo(2);
-            Assert.That(player.GetComponent<game_state>().getReputation(), Is.EqualTo(20 + (i * 3)));
+            Assert.That(gameState.getReputation(), Is.EqualTo(20 + (i * 3)));
         }
 
-        player.GetComponent<game_state>().updateReputation(-27);
+        gameState.updateReputation(-27);
         for (int i = 1; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
+            gameState.updateWellness(100);
             videoMaking.makeVideo(3);
-            Assert.That(player.GetComponent<game_state>().getReputation(), Is.EqualTo(20 + (i * 3)));
+            Assert.That(gameState.getReputation(), Is.EqualTo(20 + (i * 3)));
         }
 
-        player.GetComponent<game_state>().updateReputation(-27);
+        gameState.updateReputation(-27);
         for (int i = 1; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
+            gameState.updateWellness(100);
             videoMaking.makeVideo(4);
-            Assert.That(player.GetComponent<game_state>().getReputation(), Is.EqualTo(20 + (i * 3)));
+            Assert.That(gameState.getReputation(), Is.EqualTo(20 + (i * 3)));
         }
 
-        player.GetComponent<game_state>().updateReputation(-27);
+        gameState.updateReputation(-27);
         for (int i = 1; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
+            gameState.updateWellness(100);
             videoMaking.makeVideo(5);
-            Assert.That(player.GetComponent<game_state>().getReputation(), Is.EqualTo(20 + (i * 3)));
+            Assert.That(gameState.getReputation(), Is.EqualTo(20 + (i * 3)));
         }
         yield return null;
     }
@@ -83,7 +84,8 @@ public class NewTestScript
         player = GameObject.FindGameObjectWithTag("MainCamera");
         computerCanvas = GameObject.Find("Computer Canvas");
         videoMaking = player.GetComponent<video_making>();
-        int subscribers = player.GetComponent<game_state>().getSubscribers();
+        game_state gameState = player.GetComponent<game_state>();
+        int subscribers = gameState.getSubscribers();
 
         Assert.IsNotNull(player);
         Assert.IsNotNull(computerCanvas);
@@ -93,57 +95,57 @@ public class NewTestScript
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            int reputation = player.GetComponent<game_state>().getReputation();
+            gameState.updateWellness(100);
+            int reputation = gameState.getReputation();
             videoMaking.makeVideo(1);
-            int newSubscribers = player.GetComponent<game_state>().getSubscribers();
+            int newSubscribers = gameState.getSubscribers();
             Assert.That(newSubscribers, Is.AtLeast(subscribers + ((reputation + 3) / 5)));
             Assert.That(newSubscribers, Is.AtMost(subscribers + ((reputation + 10 + 3) / 5)));
-           subscribers = player.GetComponent<game_state>().getSubscribers();
+           subscribers = gameState.getSubscribers();
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            int reputation = player.GetComponent<game_state>().getReputation();
+            gameState.updateWellness(100);
+            int reputation = gameState.getReputation();
             videoMaking.makeVideo(2);
-            int newSubscribers = player.GetComponent<game_state>().getSubscribers();
+            int newSubscribers = gameState.getSubscribers();
             Assert.That(newSubscribers, Is.AtLeast(subscribers + ((reputation + 6) / 5)));
             Assert.That(newSubscribers, Is.AtMost(subscribers + ((reputation + 10 + 6) / 5)));
-            subscribers = player.GetComponent<game_state>().getSubscribers();
+            subscribers = gameState.getSubscribers();
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            int reputation = player.GetComponent<game_state>().getReputation();
+            gameState.updateWellness(100);
+            int reputation = gameState.getReputation();
             videoMaking.makeVideo(3);
-            int newSubscribers = player.GetComponent<game_state>().getSubscribers();
+            int newSubscribers = gameState.getSubscribers();
             Assert.That(newSubscribers, Is.AtLeast(subscribers + ((reputation + 9) / 5)));
             Assert.That(newSubscribers, Is.AtMost(subscribers + ((reputation + 10 + 9) / 5)));
-            subscribers = player.GetComponent<game_state>().getSubscribers();
+            subscribers = gameState.getSubscribers();
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            int reputation = player.GetComponent<game_state>().getReputation();
+            gameState.updateWellness(100);
+            int reputation = gameState.getReputation();
             videoMaking.makeVideo(4);
-            int newSubscribers = player.GetComponent<game_state>().getSubscribers();
+            int newSubscribers = gameState.getSubscribers();
             Assert.That(newSubscribers, Is.AtLeast(subscribers + ((reputation + 12) / 5)));
             Assert.That(newSubscribers, Is.AtMost(subscribers + ((reputation + 10 + 12) / 5)));
-            subscribers = player.GetComponent<game_state>().getSubscribers();
+            subscribers = gameState.getSubscribers();
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            int reputation = player.GetComponent<game_state>().getReputation();
+            gameState.updateWellness(100);
+            int reputation = gameState.getReputation();
             videoMaking.makeVideo(5);
-            int newSubscribers = player.GetComponent<game_state>().getSubscribers();
+            int newSubscribers = gameState.getSubscribers();
             Assert.That(newSubscribers, Is.AtLeast(subscribers + ((reputation + 15) / 5)));
             Assert.That(newSubscribers, Is.AtMost(subscribers + ((reputation + 10 + 15) / 5)));
-            subscribers = player.GetComponent<game_state>().getSubscribers();
+            subscribers = gameState.getSubscribers();
         }
 
 
@@ -159,6 +161,8 @@ public class NewTestScript
         player = GameObject.FindGameObjectWithTag("MainCamera");
         computerCanvas = GameObject.Find("Computer Canvas");
         videoMaking = player.GetComponent<video_making>();
+        game_state gameState = player.GetComponent<game_state>();
+
 
         Assert.IsNotNull(player);
         Assert.IsNotNull(computerCanvas);
@@ -166,55 +170,55 @@ public class NewTestScript
 
         for(int i =0; i < 10;i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            double origMoney = player.GetComponent<game_state>().getMoney();
+            gameState.updateWellness(100);
+            double origMoney = gameState.getMoney();
             videoMaking.makeVideo(1);
-            int subscribers = player.GetComponent<game_state>().getSubscribers();
-            double money = player.GetComponent<game_state>().getMoney();
+            int subscribers = gameState.getSubscribers();
+            double money = gameState.getMoney();
             Assert.That(money, Is.AtLeast(((int)(subscribers*0.02)) - 5+origMoney));
             Assert.That(money, Is.AtMost(((int)(subscribers * 0.02)) + 20+origMoney));
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            double origMoney = player.GetComponent<game_state>().getMoney();
+            gameState.updateWellness(100);
+            double origMoney = gameState.getMoney();
             videoMaking.makeVideo(2);
-            int subscribers = player.GetComponent<game_state>().getSubscribers();
-            double money = player.GetComponent<game_state>().getMoney();
+            int subscribers = gameState.getSubscribers();
+            double money = gameState.getMoney();
             Assert.That(money, Is.AtLeast(((int)(subscribers * 0.02)) - 5 + origMoney));
             Assert.That(money, Is.AtMost(((int)(subscribers * 0.02)) + 20 + origMoney));
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            double origMoney = player.GetComponent<game_state>().getMoney();
+            gameState.updateWellness(100);
+            double origMoney = gameState.getMoney();
             videoMaking.makeVideo(3);
-            int subscribers = player.GetComponent<game_state>().getSubscribers();
-            double money = player.GetComponent<game_state>().getMoney();
+            int subscribers = gameState.getSubscribers();
+            double money = gameState.getMoney();
             Assert.That(money, Is.AtLeast(((int)(subscribers * 0.02)) - 5 + origMoney));
             Assert.That(money, Is.AtMost(((int)(subscribers * 0.02)) + 20 + origMoney));
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            double origMoney = player.GetComponent<game_state>().getMoney();
+            gameState.updateWellness(100);
+            double origMoney = gameState.getMoney();
             videoMaking.makeVideo(4);
-            int subscribers = player.GetComponent<game_state>().getSubscribers();
-            double money = player.GetComponent<game_state>().getMoney();
+            int subscribers = gameState.getSubscribers();
+            double money = gameState.getMoney();
             Assert.That(money, Is.AtLeast(((int)(subscribers * 0.02)) - 5 + origMoney));
             Assert.That(money, Is.AtMost(((int)(subscribers * 0.02)) + 20 + origMoney));
         }
 
         for (int i = 0; i < 10; i++)
         {
-            player.GetComponent<game_state>().updateWellness(100);
-            double origMoney = player.GetComponent<game_state>().getMoney();
+            gameState.updateWellness(100);
+            double origMoney = gameState.getMoney();
             videoMaking.makeVideo(5);
-            int subscribers = player.GetComponent<game_state>().getSubscribers();
-            double money = player.GetComponent<game_state>().getMoney();
+            int subscribers = gameState.getSubscribers();
+            double money = gameState.getMoney();
             Assert.That(money, Is.AtLeast(((int)(subscribers * 0.02)) - 5 + origMoney));
             Assert.That(money, Is.AtMost(((int)(subscribers * 0.02)) + 20 + origMoney));
         }
@@ -223,5 +227,60 @@ public class NewTestScript
     }
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
+
+    [UnityTest]
+    public IEnumerator VideoMakingWellnessLossTest()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+        computerCanvas = GameObject.Find("Computer Canvas");
+        videoMaking = player.GetComponent<video_making>();
+        game_state gameState = player.GetComponent<game_state>();
+        int wellness = gameState.getWellness();
+
+        Assert.IsNotNull(player);
+        Assert.IsNotNull(computerCanvas);
+        Assert.IsNotNull(videoMaking);
+
+        for(int i = 0; i< 10;i++)
+        {
+            gameState.updateWellness(100);
+
+            videoMaking.makeVideo(1);
+             wellness = gameState.getWellness();
+            Assert.That(wellness, Is.EqualTo((100 - (3))));
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            gameState.updateWellness(100);
+            videoMaking.makeVideo(2);
+            
+            wellness = gameState.getWellness();
+            Debug.Log(wellness);
+            Assert.That(wellness, Is.EqualTo(100 - (6)));
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            gameState.updateWellness(100);
+            videoMaking.makeVideo(1);
+            wellness = gameState.getWellness();
+            Assert.That(wellness, Is.EqualTo(100 - (3 * 3)));
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            gameState.updateWellness(100);
+            videoMaking.makeVideo(4);
+            wellness = gameState.getWellness();
+            Assert.That(wellness, Is.EqualTo(100 - (3 * 4)));
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            gameState.updateWellness(100);
+            videoMaking.makeVideo(5);
+            wellness = gameState.getWellness();
+            Assert.That(wellness, Is.EqualTo(100 - (3 * 5)));
+        }
+
+        yield return null;
+    }
 
 }
