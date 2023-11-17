98dd6343ee799acf7b51057c23e856dbb7c424ba
DrakeMitchell <mitchelld7373@my.uwstout.edu>
2023-11-17 10:48:19 -0600

    fixed the rep test and working on game state

--- Assets/Tests/GameStateTesting.cs
+++ Assets/Tests/GameStateTesting.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
@@ -37,8 +38,37 @@ public class GameStateTesting
         Assert.AreEqual(gameState.getSubscribers() , 1000);
         Assert.AreEqual(gameState.getEnding() , 0);
         Assert.AreEqual(gameState.getMoney() , 100.00);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator GameStateUpdateTesting()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+        gameState = player.GetComponent<game_state>();
+        Assert.IsNotNull(player);
+        Assert.IsNotNull(gameState);
 
 
+        //Testing time
+        gameState.updateTime(0);
+        Assert.That(gameState.getTime(), Is.EqualTo(480));
+        //Testing Day Advance
+        gameState.updateTime(960);
+        Assert.That(gameState.getDay(), Is.EqualTo(2));
+        Assert.That(gameState.getTime(), Is.EqualTo(0));
+
+        gameState.updateTime(240);
+        Assert.That(gameState.getTime(), Is.EqualTo(240));
+
+        //Testing Force Sleep
+        gameState.updateTime(1);
+        Assert.That(gameState.getDay(), Is.EqualTo(2));
+        Assert.That(gameState.getTime(), Is.EqualTo(480));
+
+        //Testing hunger
+        gameState.updateTime(360);
+        //Assert.AreSame(gameState)
 
         yield return null;
     }
