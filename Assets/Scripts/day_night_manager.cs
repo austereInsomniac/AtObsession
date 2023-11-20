b9b940f34393d7ee23be53a7637756f91077bf35
Mackenzie Kuska <silverwood135831@gmail.com>
2023-11-20 13:54:09 -0600

    day/night

--- Assets/Scripts/day_night_manager.cs
+++ Assets/Scripts/day_night_manager.cs
@@ -9,12 +9,32 @@ public class day_night_manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     state = GameObject.Find("Player").GetComponent<game_state>();
+        state = GameObject.Find("Player").GetComponent<game_state>();
+        state.addOnTimeChange(updateFilter);
+
+        updateFilter(state.getTime(), state.getTime());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void updateFilter(int oldT, int newT)
     {
-        
+        UnityEngine.Color color = this.GetComponent<SpriteRenderer>().color;
+
+        if (newT >= 19 * 60 || newT < 8 * 60)
+        {
+            
+            color.a = 50f/255;
+            Debug.Log(color.a);
+        }
+        else if(newT > 17 * 60)
+        {
+            color.a = (float)((newT - (17*60)) / 612f);
+            Debug.Log(color.a);
+        }
+        else
+        {
+            color.a = 0;
+        }
+
+        this.GetComponent<SpriteRenderer>().color = color;
     }
 }
