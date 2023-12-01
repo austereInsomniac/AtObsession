d600a38ca83cc7f784740ab42b9fe318d7df5ecf
Mackenzie Kuska <silverwood135831@gmail.com>
2023-12-01 10:23:08 -0600

    shopping checkpoint

--- Assets/Scripts/shop.cs
+++ Assets/Scripts/shop.cs
@@ -62,7 +62,7 @@ public class shop : MonoBehaviour
             new buyableItem(10, 5, -50, "Microphone", true, false),
             new buyableItem(25, 10, -100, "PC Upgrade", false, false),
             new buyableItem(15, 7, -75, "Monitor", true, false),
-            new buyableItem(20, 5, -80, "Gamer Chair", true, false),
+            new buyableItem(20, 5, -80, "Chair", true, false),
             new buyableItem(10, 5, -25, "Headphones", true, false),
             new buyableItem(10, 5, -30, "New Outfit", false, true),
             new buyableItem(10, 5, -40, "New Game", false, true)
@@ -91,9 +91,9 @@ public class shop : MonoBehaviour
 
         if (item.hasSprite())
         {
-/*            string search = item.getName() + "2";
-            GameObject.Find(item.getName()).SetActive(false);
-            GameObject.Find(search).GetComponent<SpriteRenderer>().enabled = true;*/
+            string search = item.getName() + "2";
+            GameObject.Find(item.getName()).GetComponent<SpriteRenderer>().enabled = true;
+            GameObject.Find(search).GetComponent<SpriteRenderer>().enabled = true;
         }
     }
 
