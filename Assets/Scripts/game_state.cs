67af55d3046f7f3342ba802ce58e0dc454b4d3d0
Mackenzie Kuska <silverwood135831@gmail.com>
2023-12-15 07:53:17 -0600

    time

--- Assets/Scripts/game_state.cs
+++ Assets/Scripts/game_state.cs
@@ -449,7 +449,7 @@ public class game_state : MonoBehaviour
         }
 
         // player is tired
-        if (tired() && time >= (60 * 22))
+        if (tired())
         {
             // display icon
             sleepHUD.color = color2;
